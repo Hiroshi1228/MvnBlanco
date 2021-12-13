066026a93d01bbc89d053850a6da39ff4a916aa8
Hiroshi1228 <juanpablo.red.ranger@hotmail.com>
2021-12-13 14:46:26 -0600

    pregunta 4

--- practicas-app/src/main/java/uv/mx/sistemasweb/App.java
+++ practicas-app/src/main/java/uv/mx/sistemasweb/App.java
@@ -1,12 +1,35 @@
 package uv.mx.sistemasweb;
-
 import static spark.Spark.*;
 
+/**
+ * Hello world!
+ *
+ */
 public class App 
 {
-    public class HelloWorld {
-        public static void main(String[] args) {
-            get("/aprobado", (req, res) -> "8");
-        }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        get("/aprobado", (request, response) -> {
+            return "Mi estimado profesor, a pesar de que me ha costado entender la materia, realmente espero sacar un 8, si usted cree que merezco mas, es completamente libre de asignarme un 10, tenga buen inicio de semana <3";
+        });
     }
 }
